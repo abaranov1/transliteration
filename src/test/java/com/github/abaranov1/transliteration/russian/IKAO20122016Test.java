cdd6bb2e3872300753325f2d1ef1673415be569a
Александр Баранов <aleksandr@Aleksandrs-MacBook-Air.local>
2021-03-25 09:35:46 +0300

    some changes in tests

--- src/test/java/com/github/abaranov1/transliteration/russian/IKAO20122016Test.java
+++ src/test/java/com/github/abaranov1/transliteration/russian/IKAO20122016Test.java
@@ -4,29 +4,34 @@ import com.github.abaranov1.transliteration.TransliterationType;
 import com.github.abaranov1.transliteration.Transliterator;
 import com.github.abaranov1.transliteration.TransliteratorFactory;
 import com.github.abaranov1.transliteration.exception.IllegalLetterException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class IKAO20122016Test {
+
+    static Transliterator transliterator;
+
+    @BeforeAll
+    static void createTransliterator(){
+        transliterator = TransliteratorFactory.createTransliteratorByType(TransliterationType.RUSSIAN_IKAO_2012_2016);
+    }
+
     @Test
     void simpleTest(){
-        Transliterator transliterator = TransliteratorFactory.createTransliteratorByType(TransliterationType.RUSSIAN_IKAO_2012_2016);
-        String expected = "Privet!";
         String actual = transliterator.transliterate("Привет!");
-        assertEquals(expected, actual);
+        assertEquals("Privet!", actual);
     }
 
     @Test
     void allAlphabet(){
-        Transliterator transliterator = TransliteratorFactory.createTransliteratorByType(TransliterationType.RUSSIAN_IKAO_2012_2016);
         String alphabet = "абвгдеёжзиклмнопрестуфхцчшщьыъэюя";
         assertDoesNotThrow(()->transliterator.transliterate(alphabet));
     }
 
     @Test
     void notCharactersWereNotTransliterated(){
-        Transliterator transliterator = TransliteratorFactory.createTransliteratorByType(TransliterationType.RUSSIAN_IKAO_2012_2016);
         String expected = "$5 - $^&*";
         String actual = transliterator.transliterate(expected);
         assertEquals(expected, actual);
@@ -34,15 +39,12 @@ class IKAO20122016Test {
 
     @Test
     void unexpectedCharactersError(){
-        Transliterator transliterator = TransliteratorFactory.createTransliteratorByType(TransliterationType.RUSSIAN_IKAO_2012_2016);
         assertThrows(IllegalLetterException.class, () -> transliterator.transliterate("Apple"));
     }
 
     @Test
     void multiCharCharactersCapitalization(){
-        Transliterator transliterator = TransliteratorFactory.createTransliteratorByType(TransliterationType.RUSSIAN_IKAO_2012_2016);
-        String expected = "Shchuka";
         String actual = transliterator.transliterate("Щука");
-        assertEquals(expected, actual);
+        assertEquals("Shchuka", actual);
     }
 }
